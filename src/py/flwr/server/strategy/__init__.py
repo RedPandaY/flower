d067648d45d05d776555c99ad0d7513108d1d6ee
RedPandaY <zehniyazan@gmail.com>
2025-03-01 15:03:23 +0100

    init updated

--- src/py/flwr/server/strategy/__init__.py
+++ src/py/flwr/server/strategy/__init__.py
@@ -34,6 +34,7 @@ from .fault_tolerant_fedavg import FaultTolerantFedAvg as FaultTolerantFedAvg
 from .fedadagrad import FedAdagrad as FedAdagrad
 from .fedadam import FedAdam as FedAdam
 from .fedavg import FedAvg as FedAvg
+from .fedavg_dc import FedAvgWithDC as FedAvgDC
 from .fedavg_android import FedAvgAndroid as FedAvgAndroid
 from .fedavgm import FedAvgM as FedAvgM
 from .fedmedian import FedMedian as FedMedian
@@ -60,6 +61,7 @@ __all__ = [
     "FedAdagrad",
     "FedAdam",
     "FedAvg",
+    "FedAvgDC",
     "FedAvgAndroid",
     "FedAvgM",
     "FedMedian",
